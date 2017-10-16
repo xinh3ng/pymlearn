60767044dcc2f57b3c8b5258930b3635e6a0d7de
Xin Heng <xin.heng@gmail.com>
2017-10-15 22:15:02 -0700

    Added several sampling functions

--- pymlearn/sampling.py
+++ pymlearn/sampling.py
@@ -1,6 +1,7 @@
 """ML related data util functions
 
 """
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
@@ -21,3 +22,44 @@ def split_train_validate(data, train_size=0.8, shuffle=False, seed=None):
 
     return({"train": train_data,
             "validate": val_data})
+
+
+def down_sample(data, label_col, pos_size=0.5, seed=None):
+    """Down-sample the negative samples in the original imbalanced data
+
+    :param data:
+    :param label_col:
+    :param pos_size: Size (percentage) of the positive samples desired
+    :param seed:
+    :return:
+    """
+    pos_data = data[data[label_col] == 1].copy()  # positive samples
+    pos_length = len(pos_data)
+
+    neg_data = data[data[label_col] == 0].copy()  # negative samples
+    neg_length = (1.0 - pos_size) / pos_size * pos_length
+
+    neg_data = neg_data.sample(n=neg_length, replace=False, random_state=seed)
+
+    data = pd.concat([pos_data, neg_data]).sample(frac=1, replace=False).reset_index()
+    return data
+
+
+def up_sample_naive(data, label_col, pos_size = 0.5, seed=None):
+    """Up-sample the postive samples in the original imbalanced data using naive method
+
+    :param data:
+    :param label_col:
+    :param pos_size: Size (percentage) of the positive samples desired
+    :param seed:
+    :return:
+    """
+    neg_length = sum(data[label_col] == 0)
+
+    pos_length = pos_size / (1.0 - pos_size) * neg_length
+    pos_data = data[data[label_col] == 1].copy()  # positive samples
+    assert pos_length >= len(pos_data), "Required positive sample length cannot be smaller than the current"
+
+    pos_data_new = pos_data.sample(n=pos_length - len(pos_data), replace=True)
+    data = pd.concat([data, pos_data_new]).sample(frac=1, replace=False).reset_index()
+    return data
