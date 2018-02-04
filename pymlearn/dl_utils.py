7f72ddfb1531ae58eb37d48cccdc7aa4653eaf7d
Xin Heng <xin.heng@gmail.com>
2018-02-03 22:41:36 -0800

    fix a bug

--- pymlearn/dl_utils.py
+++ pymlearn/dl_utils.py
@@ -1,9 +1,9 @@
 ﻿"""
 """
 from pdb import set_trace as debug
-import resource
 from keras.callbacks import Callback
 from pydsutils.generic import create_logger
+from pydsutils.system import get_memory_usage
 
 logger = create_logger(__name__)
 
@@ -11,9 +11,25 @@ logger = create_logger(__name__)
 class TfMemoryUsage(Callback):
     """Keras callback on memory usage
     """
-    def on_epoch_end(self, logs={}):
-        print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+    def on_train_begin(self, logs={}):
+        self.mem_usage = []
+    
+    def on_epoch_begin(self, epoch, logs={}):
+        denom = 1024.0
+        usage = get_memory_usage() / denom
+        self.mem_usage.append('on_epoch_begin: %.2f Gb' % usage)
+        logger.info('on_epoch_begin: memory usage: %.2f Gb' % usage)
 
+    def on_epoch_end(self, epoch, logs={}):
+        denom = 1024.0
+        usage = get_memory_usage() / denom
+        self.mem_usage.append('on_epoch_end: %.2f Gb' % usage)
+        logger.info('on_epoch_end: memory usage: %.2f Gb' % usage)
 
-    def on_batch_end(self, logs={}):
-        print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+    def on_batch_end(self, batch, logs={}):
+        return
+        denom =  1024.0
+        usage = get_memory_usage() / denom
+        self.mem_usage.append('on_batch_end: %.2f Gb' % usage)
+        logger.info('on-batch_end: memory usage: %.2f Gb' % usage)
+
